7b45c72f3f8fd8beb226a5f6a75b8a5a8964121a
Darya Osipova <domali555@yandex.ru>
2023-04-23 01:54:47 +0300

    new commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const router = require('./routes/index');
 
 const app = express();
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 app.use(cors());
 
 app.use(bodyParser.json());
